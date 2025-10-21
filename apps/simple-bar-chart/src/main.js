06919fab8ddb1fc4e4bf069cfde0bd27e9e7c5c3
Erano <muhammedalisert01@gmail.com>
2025-10-22 02:57:07 +0300

    Feat: Simple Bar Chart added as demo

---
+++ apps/simple-bar-chart/src/main.js
@@ -0,0 +1,7 @@
+// Main JavaScript file for Float Test
+console.log('Bar chart Project Started!');
+
+// You can add interactive JavaScript here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM loaded and ready!');
+});
